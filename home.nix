1f3ef405f4b977cb131335063bad93f43fd5990b
Jan Andersson <jan.andersson@soundtrap.com>
2024-12-19 01:11:07 +0100

    Tweaks, add tree

--- home.nix
+++ home.nix
@@ -15,11 +15,19 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    # Programming
     nixpkgs-fmt
     nodejs_18
     cmake
+
+    # Mac Apps
     spotify
+
+    # Fonts
     nerd-fonts.fira-code
+
+    # CLI utils
+    tree
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
@@ -28,12 +36,6 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
-
-  # fonts.fontconfig = {
-  #   enable = true;
-  #   defaultFonts.monospace = [ "Fira Code" ];
-  # };
-
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
